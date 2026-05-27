de2608ac03014df1172aef677262f2c988505c85
Patricia <patquesa@Inception.patquesa.42.fr>
2026-05-27 17:48:44 +0200

    wordpress

--- srcs/requirements/mariadb/tools/setup.sh
+++ srcs/requirements/mariadb/tools/setup.sh
@@ -9,8 +9,9 @@ if [ ! -d "/var/lib/mysql/mysql" ]; then
     mysql_install_db --user=mysql --datadir=/var/lib/mysql
 fi
 
-# ARRANCAR MariaDB temporalmente (IMPORTANTE)
-mysqld_safe &
+# arrancar temporal SOLO para crear DB y user
+mysqld --user=mysql --datadir=/var/lib/mysql &
+pid="$!"
 
 #esperar a que arranque
 until mariadb-admin ping -h localhost --silent; do
@@ -22,6 +23,9 @@ mariadb -e "CREATE DATABASE IF NOT EXISTS wordpress;"
 mariadb -e "CREATE USER IF NOT EXISTS 'wp_user'@'%' IDENTIFIED BY '1234';"
 mariadb -e "GRANT ALL PRIVILEGES ON wordpress.* TO 'wp_user'@'%';"
 mariadb -e "FLUSH PRIVILEGES;"
+
+kill -TERM "$pid"
+wait "$pid" 2>/dev/null
 
 #Arrancar MariaDB en foreground
 exec mysqld
--- srcs/requirements/wordpress/tools/setup.sh
+++ srcs/requirements/wordpress/tools/setup.sh
@@ -8,9 +8,27 @@ mkdir -p /var/www/html
 
 # descargar WordPress
 curl -o wordpress.tar.gz https://wordpress.org/latest.tar.gz
+
+#descomprimir WordPress
 tar -xzf wordpress.tar.gz -C /var/www/html --strip-components=1
+
+# crear wp-config.php
+cp /var/www/html/wp-config-sample.php /var/www/html/wp-config.php
+
+#configurar WordPress
+sed -i "s/database_name_here/wordpress/" /var/www/html/wp-config.php
+sed -i "s/username_here/wp_user/" /var/www/html/wp-config.php
+sed -i "s/password_here/1234/" /var/www/html/wp-config.php
+sed -i "s/localhost/mariadb/" /var/www/html/wp-config.php
+
+# esperar MariaDB
+until mariadb -h mariadb -u wp_user -p1234 -e "SELECT 1" 2>/dev/null; do
+    echo "Waiting for MariaDB..."
+    sleep 2
+done
 
 #permisos
 chown -R www-data:www-data /var/www/html
 
+#arrancar php-fpm
 exec php-fpm7.4 -F
