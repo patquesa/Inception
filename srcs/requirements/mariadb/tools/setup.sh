28df862af0ea095639a0ddd2a1267a91f708044a
patquesa <patquesa@DESKTOP-LFLJTG9>
2026-06-01 16:04:17 +0200

    push

--- srcs/requirements/mariadb/tools/setup.sh
+++ srcs/requirements/mariadb/tools/setup.sh
@@ -14,22 +14,22 @@ if [ ! -d "/var/lib/mysql/${MYSQL_DATABASE}" ]; then
 
     echo "Configurando MariaDB por primera vez..."
 
-# arrancar temporal SOLO para crear DB y user
-mysqld --user=mysql --datadir=/var/lib/mysql &
-pid="$!"
+	# arrancar temporal SOLO para crear DB y user
+	mysqld --user=mysql --datadir=/var/lib/mysql &
+	pid="$!"
 
-#esperar a que arranque
-until mariadb-admin ping -h localhost --silent; do
-    sleep 1
-done
+	#esperar a que arranque
+	until mariadb-admin ping -h localhost --silent; do
+		sleep 1
+	done
 
-# crear base de datos y usuario (Usando Heredoc para evitar cortes de conexión)
-    mariadb << EOF
-CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;
-CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
-GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO '${MYSQL_USER}'@'%';
-ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
-FLUSH PRIVILEGES;
+	# crear base de datos y usuario (Usando Heredoc para evitar cortes de conexión)
+		mariadb << EOF
+	CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;
+	CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
+	GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO '${MYSQL_USER}'@'%';
+	ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
+	FLUSH PRIVILEGES;
 EOF
 
 kill -TERM "$pid"
