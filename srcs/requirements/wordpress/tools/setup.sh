fcd94daa89249fa065c4675db68c9ebaf49dc07f
patquesa <patquesa@DESKTOP-LFLJTG9>
2026-06-07 14:23:45 +0200

    correccion nombre archivo

--- srcs/requirements/wordpress/tools/setup.sh
+++ srcs/requirements/wordpress/tools/setup.sh
@@ -44,6 +44,7 @@ cd /var/www/html
 if ! wp core is-installed --allow-root; then
     wp core install \
         --url="${DOMAIN_NAME}" \
+		#--url="https://${DOMAIN_NAME}" COMPROBAR
         --title="${WP_TITLE}" \
         --admin_user="${WP_ADMIN_USER}" \
         --admin_password="${WP_ADMIN_PASSWORD}" \
