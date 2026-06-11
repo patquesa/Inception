b392cfaa7854a09d5991f2c89dd31db0d8ad71fb
Patricia <patquesa@Inception.patquesa.42.fr>
2026-06-11 10:36:31 +0200

    english comments

--- srcs/requirements/mariadb/tools/setup.sh
+++ srcs/requirements/mariadb/tools/setup.sh
@@ -1,35 +1,35 @@
 #!/bin/bash
 
-# Extraemos el contenido de los secretos y los guardamos en variables locales
+# We extract the contents of the secrets and store them in local variables
 MYSQL_ROOT_PASSWORD=$(cat /run/secrets/db_root_password)
 MYSQL_PASSWORD=$(cat /run/secrets/db_password)
 
 unset MYSQL_HOST
 
-#Crear carpeta necesaria para MariaDB
+# Create the necessary folder for MariaDB
 mkdir -p /run/mysqld
 chown -R mysql:mysql /run/mysqld
 
-#Inicializa mariadb
+# Initialize mariadb
 if [ ! -d "/var/lib/mysql/mysql" ]; then
     mysql_install_db --user=mysql --datadir=/var/lib/mysql
 fi
 
-# Solo configuramos si NO existe nuestra base de datos de WordPress
+# We only configure if our WordPress database does NOT exist.
 if [ ! -d "/var/lib/mysql/${MYSQL_DATABASE}" ]; then
 
     echo "Configurando MariaDB por primera vez..."
 
-	# arrancar temporal SOLO para crear DB y user
+	# Start temporary ONLY to create DB and user
 	mysqld --user=mysql --datadir=/var/lib/mysql --skip-name-resolve --skip-networking &
 	pid="$!"
 
-	# Esperar a que arranque usando el protocolo de socket local (inmune a bloqueos de IP/Host)
+	# Wait for it to boot using the local socket protocol (immune to IP/Host blocks)
 	until mariadb-admin --protocol=SOCKET --socket=/run/mysqld/mysqld.sock ping --silent; do
         sleep 1
     done
 
-	# crear base de datos y usuario (Usando Heredoc para evitar cortes de conexión)
+	# Create database and user (Using Heredoc to avoid connection cuts)
 	mariadb --protocol=SOCKET --socket=/run/mysqld/mysqld.sock << EOF
 CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;
 CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
@@ -46,5 +46,5 @@ else
     echo "La base de datos ya existe en el volumen. Saltando configuración."
 fi
 
-#Arrancar MariaDB en foreground
+# Start MariaDB in foreground
 exec mysqld --user=mysql --datadir=/var/lib/mysql --bind-address=0.0.0.0 --skip-name-resolve
